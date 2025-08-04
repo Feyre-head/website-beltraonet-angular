32d44b43b82afcc1f3fef7c9468d8aa2ea43395f
Inara Oenning <inaraoenning@gmail.com>
2025-08-04 13:51:46 -0300

    Removendo componenete CTA

--- src/app/views/home-pages/single/single-home3/components/home3-faq/home3-faq.component.ts
+++ src/app/views/home-pages/single/single-home3/components/home3-faq/home3-faq.component.ts
@@ -14,19 +14,27 @@ import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 export class Home3FaqComponent {
     faqs: FaqType[] = [
         {
-            question: 'Como faço para contratar um plano de internet da Beltraonet?',
+            question: 'Área do Cliente',
             answer: 'Você pode contratar um plano diretamente pelo nosso site, WhatsApp, telefone ou visitando nossa loja física. Também temos consultores disponíveis para te ajudar a escolher o plano ideal.'
         },
         {
-            question: 'Qual é o prazo para instalação após a contratação?',
+            question: 'Comercial WhatsApp',
             answer: 'Normalmente realizamos a instalação em até 48 horas úteis após a confirmação do pedido, dependendo da disponibilidade técnica na sua área.'
         },       
         {
-            question: 'Quais são os horários de funcionamento do suporte?',
+            question: 'Reclamações WhatsApp',
             answer: 'O atendimento telefônico funciona todos os dias, das 8h às 20h beltraonet.com.br; fora desse período, você pode enviar e‑mail.'
         },
         {
-            question: 'Posso acompanhar o status de uma solicitação de suporte?',
+            question: 'Suporte Técnico WhatsApp',
+            answer: ''
+        },
+        {
+            question: 'Nossa página no Facebook',
+            answer: ''
+        },
+        {
+            question: 'Nossa página no Instagram',
             answer: 'Sim. Os assinantes têm acesso à Área do Cliente no site da Beltraonet, onde é possível consultar boletos e acompanhar solicitações de suporte. Para isso, acesse o portal da empresa e faça login na Central do Assinante (se ainda não tiver cadastro, é necessário criar um).'
         }
     ];
