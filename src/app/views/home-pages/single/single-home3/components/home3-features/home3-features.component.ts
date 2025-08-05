2f4c60a698c2936ad65132ab5b6ba035b2382404
Inara Oenning <inaraoenning@gmail.com>
2025-08-05 14:46:32 -0300

    Ajuste bs5

--- src/app/views/home-pages/single/single-home3/components/home3-features/home3-features.component.ts
+++ src/app/views/home-pages/single/single-home3/components/home3-features/home3-features.component.ts
@@ -4,7 +4,7 @@ import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-home3-features',
    host: { 'data-component-id': 'home3-feature-instance' },
-  imports: [RouterLink],
+  
   templateUrl: './home3-features.component.html',
   styles: ``
 })
